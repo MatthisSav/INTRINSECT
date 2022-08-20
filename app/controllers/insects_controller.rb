a8caf9c6a2989be41ad36f45a94cd802ed66cdc7
MK <publicelo@yahoo.com.au>
2022-08-20 16:34:24 +0200

    add_new_and_create_insect_controller

--- app/controllers/insects_controller.rb
+++ app/controllers/insects_controller.rb
@@ -1,6 +1,18 @@
 class InsectsController < ApplicationController
+  def new
+    # @types = [""]
+    @insect = Insect.new
+  end
 
-  def index
-    
+  def create
+    @insect = Insect.new(insect_params)
+    @insect.user = current_user
+    @insect.save
+  end
+
+  private
+
+  def insect_params
+    params.require(:insect).permit(:name, :description, :type)
   end
 end
