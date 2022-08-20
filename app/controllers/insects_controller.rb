932c9176118b15e453a8bd0dbe73b7da983686e3
Matthis Savagnac <msavagnac@gmail.com>
2022-08-20 16:28:31 +0200

    added edit, update & destroy for insect controller

--- app/controllers/insects_controller.rb
+++ app/controllers/insects_controller.rb
@@ -1,6 +1,26 @@
 class InsectsController < ApplicationController
 
-  def index
-    
+  def edit
+    @insect = Insect.find(params[:id])
+  end
+
+  def update
+    @insect = Insect.find(params[:id])
+    @insect.update(insect_params)
+
+    redirect_to insect_path(@insect)
+  end
+
+  def destroy
+    @insect = Insect.find(params[:id])
+    @insect.destroy
+
+    redirect_to insect_path
+  end
+
+  private
+
+  def insect_params
+    params.require(:insect).permit(:name, :description, :type)
   end
 end
