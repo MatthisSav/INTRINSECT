0d7bbb2f3d854a915f47d23896526f18617a320d
MK <publicelo@yahoo.com.au>
2022-08-25 20:49:21 +0200

    adding active storage

--- app/models/insect.rb
+++ app/models/insect.rb
@@ -2,4 +2,6 @@ class Insect < ApplicationRecord
   TYPES = ["Aptérygotes", "Blattoptères", "Coléoptères", "Dermaptères", "Diptères", "Embioptères", "Éphéméroptères", "Grylloblattoptères", "Hémiptères", "Hyménoptères", "Blattoptères", "Lépidoptères", "Mantophasmatoptères", "Mantoptères", "Mécoptères", "Mégaloptères", "Neuroptères", "Odonates", "Orthoptères", "Phasmatoptères", "Phthiraptères", "Plécoptères", "Psocoptères", "Raphidioptères", "Siphonaptères", "Strepsiptères", "Thysanoptères", "Thysanoures", "Trichoptères", "Zoraptères"]
 
   belongs_to :user
+
+  has_one_attached :photo
 end
