631ead8dd672578eedc585b679fb25b62066ade7
Raphael Zimeray <raphaelzimeray@me.com>
2022-08-27 11:06:30 +0200

    mvcbooking

--- app/models/insect.rb
+++ app/models/insect.rb
@@ -1,5 +1,5 @@
 class Insect < ApplicationRecord
   TYPES = ["Aptérygotes", "Blattoptères", "Coléoptères", "Dermaptères", "Diptères", "Embioptères", "Éphéméroptères", "Grylloblattoptères", "Hémiptères", "Hyménoptères", "Blattoptères", "Lépidoptères", "Mantophasmatoptères", "Mantoptères", "Mécoptères", "Mégaloptères", "Neuroptères", "Odonates", "Orthoptères", "Phasmatoptères", "Phthiraptères", "Plécoptères", "Psocoptères", "Raphidioptères", "Siphonaptères", "Strepsiptères", "Thysanoptères", "Thysanoures", "Trichoptères", "Zoraptères"]
-
+  has_many :bookings
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_20_145248) do
+ActiveRecord::Schema.define(version: 2022_08_25_174434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "booking_start_date"
+    t.datetime "booking_end_date"
+    t.bigint "user_id", null: false
+    t.bigint "insect_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["insect_id"], name: "index_bookings_on_insect_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "insects", force: :cascade do |t|
     t.string "name"
@@ -37,5 +48,7 @@ ActiveRecord::Schema.define(version: 2022_08_20_145248) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "insects"
+  add_foreign_key "bookings", "users"
   add_foreign_key "insects", "users"
 end
